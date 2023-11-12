502b6f27cf4d5f6a731a1c16c2e7fa8478e52da7
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-13 00:48:26 +0200

    updated

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+int _printf(const char *format, ...)
+{
+ va_list ptr;
+ va_start(ptr, format);
+ int counter = 0;
+ char specifier;
+ if (format != NULL)
+ {
+  while (*format)
+  {
+   if (*format != '%')
+   {
+    write(1, &(*format), 1);
+    counter += 1;
+   }
+   else
+   {
+    specifier = *(++format);
+    if (specifier == '%')
+    {
+     write(1, &specifier, 1);
+     counter += 1;
+    }
+    if (specifier == 's')
+    counter += write_string(va_arg(ptr, char *));
+    if (specifier == 'c')
+     counter += write_char(va_arg(ptr, char));
+    else
+     return -1;
+   }
+   format++;
+  }
+ }
+ va_end(ptr);
+}
+

0b01ebb83a981cbb9a022f61e5c7b69e033d88ee
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-12 23:00:42 +0200

    updated

--- main.h
+++ main.h
@@ -7,7 +7,14 @@
 #include<stdarg.h>
 #include<stdlib.h>
 
-
+int _printf(const char *format, ...);
+int write_char(char str);
+int write_string(char* str);
+void write_int(int num);
+void write_binary(int num);
+int write_reverse(char* str);
+int write_rot13(char* str);
+void write_decimal(int num);
 
 
 #endif

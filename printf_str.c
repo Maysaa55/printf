cb56e358fca6d999d78ba523841c7e0a53352d23
DanKaroum <dan.karoum5@gmail.com>
2023-11-12 14:40:12 -0800

    updated

---
+++ printf_str.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * write_string - print a string
+ * @str : string pointer
+ * Return: string length
+ */
+int write_string(char  *str)
+{
+	write(1, str, strlen(str));
+	return (strlen(str));
+}

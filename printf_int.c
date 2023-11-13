65f5bd6c67247f7605c9188a334b6ec2d1157f3b
DanKaroum <dan.karoum5@gmail.com>
2023-11-13 13:45:43 -0800

    updated

--- printf_int.c
+++ printf_int.c
@@ -7,47 +7,49 @@
  *
  * Return - nothing
  */
-
-void to_string(char str[],int num)
+void to_string(char str[], int num)
 {
 	int i, tmp, length, n;
-	
+
 	length = 0;
-	if(num < 0) 
+	if (num < 0)
 	{
-		n=(-1) *(num);
-		num = (-1) *num;
+		n = (-1) * (num);
+		num = (-1) * num;
 	}
 	else
 	{
-		n=num;
+		n = num;
 	}
-	while(n !=0 )
+	while (n != 0)
 	{
 		length++;
 		n /= 10;
 	}
-	for ( i=0;i<length;i++)
+	for (i = 0; i < length; i++)
 	{
 		tmp = num % 10;
 		num = num / 10;
-		str[length - (i+1)] = tmp + '0';
+
+		str[length - (i + 1)] = tmp + '0';
 	}
 	str[length] = '\0';
 }
+
 /**
- * write_int  - prints an integer .
+ * write_int - prints an integer
  * @num: the number to print
  *
- * Return - nothing .
+ * Return - nothing
  */
 void write_int(int num)
 {
 	char *str;
 	char negative;
+
 	negative = '-';
-	str =(char*) malloc(20 *sizeof(char));
-	to_string(str ,num);
+	str = (char *) malloc(20 * sizeof(char));
+	to_string(str, num);
 	if (num < 0)
 		write(1, &negative, 1);
 	write(1, str, strlen(str));
--- printf_reverse.c
+++ printf_reverse.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * write_reverse  - prints the reverse string .
- * @str: pointer to the string to reverse 
- *
- * Return: the number of printed characters .
+ *write_reverse - prints the reverse string
+ *@str : pointer to the string to reverse
+ *Return value : strlen -  the number of printed characters
  */
+
 int write_reverse(char *str)
 {
-int i ;
-for (i =strlen(str)-1; i>= 0 ;i++)
-{
-write(1, &str[i] , 1);
-}
-return strlen(str);
+	int i;
+
+	for (i = strlen(str) - 1; i >= 0; i++)
+	{
+		write(1, &str[i], 1);
+	}
+	return (strlen(str));
 }
 

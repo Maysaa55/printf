21e81307c0e6264119d9b4ae47c34622bad6764c
DanKaroum <dan.karoum5@gmail.com>
2023-11-13 15:53:05 -0800

    updated

--- _printf.c
+++ _printf.c
@@ -21,6 +21,7 @@ int _printf(const char *format, ...)
     if (*format == '\\')
     {
      if (*(format + 1) == 'n')
+	     counter += 1;
      {
       write_char('\n');
      }
@@ -43,12 +44,12 @@ int _printf(const char *format, ...)
      counter += write_string(va_arg(ptr, char *));
     else if (specifier == 'c')
      counter += write_char(va_arg(ptr, int));
-    else if(specifier == 'i' || specifier == 'd')
-      write_int(va_arg(ptr, int));
     else if(specifier == 'r')
 	counter += write_reverse(va_arg(ptr, char*));
     else if(specifier == 'R')
 	counter += write_rot13(va_arg(ptr, char*));
+    else if(specifier == 'd')
+	    write_double(va_arg(ptr,int));
     else
      write(1, &specifier, 1);
    }

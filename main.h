c91faf4a5a2f3d1dec3901bc463c4e33e8d15e07
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-13 14:59:03 +0200

    updated

--- main.h
+++ main.h
@@ -6,16 +6,17 @@
 #include<unistd.h>
 #include<stdarg.h>
 #include<stdlib.h>
+#include<limits.h>
 
 int _printf(const char *format, ...);
 int write_char(int str);
-int write_string(char* str);
+int write_string(char *str);
 void write_int(int num);
-void write_binary(int num);
-int write_reverse(char* str);
-int write_rot13(char* str);
+void write_binary(unsigned int num);
+int write_reverse(char *str);
+int write_rot13(char *str);
 void write_decimal(int num);
-
+void to_string(char str[], int num);
 
 #endif
 

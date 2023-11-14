f7a8c2ac34ee50ce1cf30da05b7ffa98cfd92007
DanKaroum <dan.karoum5@gmail.com>
2023-11-14 10:07:41 -0800

    updated

--- printf_reverse.c
+++ printf_reverse.c
@@ -1,22 +1,23 @@
 #include "main.h"
 /**
- * write_reverse  - prints the reverse string .
- * @str: pointer to the string to reverse 
+ * write_reverse - prints the reverse string
+ * @str: pointer to the string to reverse
  *
- * Return: the number of printed characters .
+ * Return: the number of printed characters
  */
 int write_reverse(char *str)
 {
-int i ;
-if (str != NULL){
-for (i =strlen(str)-1; i>= 0 ;i--)
-{
-write(1, &str[i] , 1);
-}
+	int i;
 
-return (strlen(str));
-}
-else 
-return (0);
+	if (str != NULL)
+	{
+		for (i = strlen(str) - 1; i >= 0; i--)
+		{
+			write(1, str[i], 1);
+		}
+		return (strlen(str));
+	}
+	else
+		return (0);
 }
 

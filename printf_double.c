5b6fef6d3f92024f85652074e73bd8f08aaaa905
DanKaroum <dan.karoum5@gmail.com>
2023-11-14 05:57:26 -0800

    updated

--- printf_double.c
+++ printf_double.c
@@ -5,27 +5,12 @@
  *
  * Return: nothing .
  */
-int write_double(int num){
-char *str;
-int i, temp;
-char negative;
-temp = num;
-negative = '-';
-str = (char*)malloc(sizeof(char)*20);
-to_string(str,num);
-i = strlen(str);
-if (temp < 0 ){
-write(1, &negative, 1);
-write(1,str,strlen(str));
-write(1, &negative, 1);
-write(1,str,strlen(str));
-i += 1;
-}
-else{
-write(1,str,strlen(str));
-write(1,str,strlen(str));
-}
-free(str);
-return (2*i);
+int write_double(int num)
+{
+	int i;
+	i = 0;
+	i += write_int(num);
+	i += write_int(num);
+	return i;
 }
 

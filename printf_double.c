9c8bc13e32eb0f678a233a3ef097d0e54edc5295
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-14 02:19:46 +0200

    updated

--- printf_double.c
+++ printf_double.c
@@ -7,12 +7,25 @@
  */
 int write_double(int num){
 char *str;
-int i;
+int i, temp;
+temp = num;
+char negative;
+negative = '-';
 str = (char*)malloc(sizeof(char)*20);
 to_string(str,num);
+i = strlen(str);
+if (temp < 0 ){
+write(1, &negative, 1);
+write(1,str,strlen(str));
+write(1, &negative, 1);
+write(1,str,strlen(str));
+i += 1;
+}
+else{
 write(1,str,strlen(str));
 write(1,str,strlen(str));
 i = strlen(str);
+}
 free(str);
 return (2*i);
 }

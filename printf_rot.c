f95ae40c296a63f2fc2c864eab312eb8e194d70a
DanKaroum <dan.karoum5@gmail.com>
2023-11-14 09:39:39 -0800

    updated

--- printf_rot.c
+++ printf_rot.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
-* write_rot13  - prints rot13ed string .
+* write_rot13 - prints rot13ed string
 * @str: the string to cinvert
 *
-* Return: the number of printed characters .
+* Return: the number of printed characters
 */
 int write_rot13(char *str)
 {
 	char letter;
 	int counter;
+
 	counter = 0;
+
 	if (str != NULL)
 	{
 		while (*str != '\0')
@@ -17,7 +19,7 @@ int write_rot13(char *str)
 			letter = *str;
 			if ((letter >= 'A' && letter <= 'M') || (letter >= 'a' && letter <= 'm'))
 				letter += 13;
-			else if ((letter >= 'N' && letter <= 'Z') || (letter >= 'n' && letter <= 'z'))
+			if ((letter >= 'N' && letter <= 'Z') || (letter >= 'n' && letter <= 'z'))
 				letter -= 13;
 
 			write(1, &letter, 1);
